8882374f27f2f34ac60d58969f3a20a35ef2faa0
anujbolewar <bolewara@gmail.com>
2026-06-10 04:13:31 +0530

    add ecosystem.config.js for PM2 deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "mission-control",
+      name: "katalyst",
       script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: __dirname,
