c940a627a1bfaadd5855191b3b9ec787f42b55f5
anujbolewar <bolewara@gmail.com>
2026-06-10 03:55:20 +0530

    add accept/decline controls to flow chat editor

---
+++ src/components/goal-flow/flow-controls.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export function FlowControls() {
+  return (
+    <div className="flow-controls">
+      <button>Accept</button>
+      <button>Decline</button>
+    </div>
+  );
+}
