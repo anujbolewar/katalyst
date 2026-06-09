81dbdc2f58f0a42c5778588158872cc781356db0
anujbolewar <bolewara@gmail.com>
2026-06-10 03:55:20 +0530

    add goal-flow index exports for clean imports

---
+++ src/components/goal-flow/index.ts
@@ -0,0 +1,7 @@
+export * from './flow-chat';
+export * from './flow-canvas';
+export * from './tree-renderer';
+export * from './flow-controls';
+export * from './node-types';
+export * from './layout';
+export * from './edges';
